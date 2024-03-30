4faa1a347ca82a445be086dbded50ff6e63116c2
vital121 <123862035+vital121@users.noreply.github.com>
2024-03-30 15:25:19 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
