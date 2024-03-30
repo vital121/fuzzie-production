44478f0256466c8cd466db0dee36de7aaa51426c
vital121 <123862035+vital121@users.noreply.github.com>
2024-03-30 15:31:57 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
+  basePath: "/fuzzie-production/",
   reactStrictMode: true,
   images: {
     remotePatterns: [
